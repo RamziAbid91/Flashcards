1b3b617a209dc8f28ff58fe11a451527c2509e40
RamziAbid91 <juventino91@ymail.com>
2025-06-14 10:14:28 -0400

    Initial Commit

---
+++ Flashcards_Chinese/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashcards_Chinese
+//
+//  Created by Ramzi Abid on 2025-06-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashcards_Chinese/Flashcards_ChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcards_ChineseApp.swift
+//  Flashcards_Chinese
+//
+//  Created by Ramzi Abid on 2025-06-14.
+//
+
+import SwiftUI
+
+@main
+struct Flashcards_ChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
